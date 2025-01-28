4e5a0a9d5141f12d4c47eb7e2799e47df7512eca
Jorge Luis <jorjoluiso@gmail.com>
2025-01-28 17:48:35 -0500

    [REF] Upgrade code

--- get_version/models/version.py
+++ get_version/models/version.py
@@ -8,18 +8,18 @@ class Version(models.Model):
     _description = "Get Version"
 
     version = fields.Char(default="Versions of Odoo Platform")
-    version_database = fields.Char(compute="_get_version_database")
-    version_python = fields.Char(compute="_get_version_python")
-    version_os = fields.Char(compute="_get_version_os")
-    version_odoo = fields.Char(compute="_get_version_odoo")
+    version_database = fields.Char(compute="_compute_version_database")
+    version_python = fields.Char(compute="_compute_version_python")
+    version_os = fields.Char(compute="_compute_version_os")
+    version_odoo = fields.Char(compute="_compute_version_odoo")
 
     @api.depends("version")
-    def _get_version_python(self):
+    def _compute_version_python(self):
         for record in self:
             record.version_python = platform.python_version()
 
     @api.depends("version")
-    def _get_version_database(self):
+    def _compute_version_database(self):
         sql_query = """
             SELECT version() as database_version
         """
@@ -30,12 +30,12 @@ class Version(models.Model):
             record.version_database = database_version
 
     @api.depends("version")
-    def _get_version_os(self):
+    def _compute_version_os(self):
         for record in self:
             record.version_os = f"{platform.system()}  {platform.release()}"
 
     @api.depends("version")
-    def _get_version_odoo(self):
+    def _compute_version_odoo(self):
         result = self.env["ir.module.module"].search_read(
             [("name", "=", "base")], ["latest_version"]
         )
--- get_version/static/src/components/version_dashboard.js
+++ get_version/static/src/components/version_dashboard.js
@@ -1,46 +1,44 @@
-/** @odoo-module **/
+import {registry} from "@web/core/registry"
+import {useService} from "@web/core/utils/hooks"
 
-import {registry} from "@web/core/registry";
-import {useService} from "@web/core/utils/hooks";
-
-const {Component, useState, onWillStart} = owl;
+const {Component, useState, onWillStart} = owl
 
 export class OwlVersionDashboard extends Component {
     setup() {
         this.state = useState({
             title: "",
             version: {},
-        });
+        })
 
-        this.orm = useService("orm");
+        this.orm = useService("orm")
 
         onWillStart(async () => {
-            this.state.title = "Versions";
-            this.state.version = await this.getVersion();
-        });
+            this.state.title = "Versions"
+            this.state.version = await this.getVersion()
+        })
     }
 
     async getVersion() {
         try {
-            const result = await this.orm.searchRead("version.version", []);
+            const result = await this.orm.searchRead("version.version", [])
 
             return {
                 versionOdoo: result[0].version_odoo,
                 versionDatabase: result[0].version_database,
                 versionPython: result[0].version_python,
                 versionOS: result[0].version_os,
-            };
+            }
         } catch {
             return {
                 versionOdoo: "",
                 versionDatabase: "",
                 versionPython: "",
                 versionOS: "",
-            };
+            }
         }
     }
 }
 
-OwlVersionDashboard.template = "owl.OwlVersionDashboard";
+OwlVersionDashboard.template = "owl.OwlVersionDashboard"
 
-registry.category("actions").add("owl.version_dashboard", OwlVersionDashboard);
+registry.category("actions").add("owl.version_dashboard", OwlVersionDashboard)
